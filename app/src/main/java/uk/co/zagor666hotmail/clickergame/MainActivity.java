3b6e3a5ae3d5b8c63603808bb1d08a42ded4b82c
omnicrog <mr_crog@msn.com>
2018-04-17 16:51:34 +0100

    cleanup

--- app/src/main/java/uk/co/zagor666hotmail/clickergame/MainActivity.java
+++ app/src/main/java/uk/co/zagor666hotmail/clickergame/MainActivity.java
@@ -2,9 +2,9 @@ package uk.co.zagor666hotmail.clickergame;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Toast;
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     //Stops the back button being used
     @Override
