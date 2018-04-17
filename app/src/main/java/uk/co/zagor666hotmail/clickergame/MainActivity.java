2d44013734355267e507a91a2a8217768a951c2e
omnicrog <mr_crog@msn.com>
2018-04-17 13:22:13 +0100

    one small change - random advert

--- app/src/main/java/uk/co/zagor666hotmail/clickergame/MainActivity.java
+++ app/src/main/java/uk/co/zagor666hotmail/clickergame/MainActivity.java
@@ -2,14 +2,11 @@ package uk.co.zagor666hotmail.clickergame;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 
